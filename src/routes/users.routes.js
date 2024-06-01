944949f70b29475967522bc1aab5ee830f515c1e
Matias Sanchez <matiasanchez999@gmail.com>
2024-06-01 00:04:04 -0300

    fourth commit
    Creation of CRUD in router

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -3,26 +3,49 @@ import { pool } from "../db.js";
 
 const router = Router();
 
-router.get('/users', (req, res) => {
-    res.send("Obteniendo usarios")
+router.get('/users', async(req, res) => {
+    const {rows} = await pool.query("SELECT * FROM users")
+    res.status(200).json(rows)
 })
 
-router.get('/users/:id', (req, res) => {
+router.get('/users/:id', async(req, res) => {
     const {id} =req.params
-    res.send("Obteniendo usarios" + id)
+
+    const {rows} = await pool.query(`SELECT * FROM users WHERE id = ${id}`)
+
+    if(rows.length === 0) {
+        res.status(404).json({message: "no users found"})
+    }
+    res.status(200).json(rows[0])
 })
 
-router.post('/users', (req, res) => {
-    res.send("creando usarios")
+router.post('/users', async (req, res) => {
+    const data = req.body
+    const {rows} = await pool.query("INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *", [data.name, data.email])
+
+    res.status(200).json(rows[0])
 })
 
-router.delete('/users/:id', (req, res) => {
+router.delete('/users/:id', async(req, res) => {
     const {id} = req.params
-    res.send("eliminando usarios")
+
+    const { rowCount} = await pool.query(`DELETE FROM users WHERE id = ${id} RETURNING *`)
+
+    if(rowCount === 0) {
+        res.status(404).json({message: "no users found"})
+    }
+
+    return res.sendStatus(204)
 })
 
-router.put('/users/:id', (req, res) => {
-    res.send("actualizando usarios")
+router.put('/users/:id', async(req, res) => {
+    const {id} = req.params
+    const {name, email} = req.body
+
+    const {rows} = await pool.query(`UPDATE users SET name = $1, email = $2 WHERE id = $3 RETURNING *`, [name, email, id])
+
+    
+    return res.json(rows[0])
 })
 
 export default router
